8f53aab809206417ea76217ecea3f03d6b3caec9
xamdp <64017084+xamdp@users.noreply.github.com>
2026-08-09 11:53:07 +0900

    ref: use inserAdjacentElement() to specifically insert content inside the container

--- src/home.js
+++ src/home.js
@@ -1,20 +1,20 @@
 
-// maybe i should rename the file to Home.js
 
-// i need to append the title inside the div
 function Home() {
 	const contentDiv = document.querySelector("#content");
+	const adjacentHeader = document.querySelector("header");
 	const div = document.createElement("div")
 	const title = document.createElement("h2");
 	const p = document.createElement("p");
 
 	div.setAttribute("id", "home-div")
-	title.textContent = "JollyBee"
+	title.textContent = "JollyBae"
 	title.setAttribute("class", "heading");
 	p.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error aspernatur sed, expedita quae ullam dolorem quas beatae unde iure id alias porro omnis aperiam, molestias recusandae accusantium! Aliquam, quaerat doloremque!"
 
 	div.append(title, p);
 	contentDiv.append(div);
+	adjacentHeader.insertAdjacentElement("afterend", contentDiv); // targets the header and inserts #content after it
 }
 
 export { Home };
