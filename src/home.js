718f29308c519f2e7f65bb37a525e15a4fcce3b5
xamdp <64017084+xamdp@users.noreply.github.com>
2026-08-10 12:32:49 +0900

    ref: add heading-text-container class

--- src/home.js
+++ src/home.js
@@ -5,15 +5,19 @@ function Home() {
 	const div = document.createElement("div")
 	const title = document.createElement("h2");
 	const p = document.createElement("p");
+	const headingTextContainer = document.createElement("div");
 
+	headingTextContainer.classList.add("heading-text-container")
 	div.setAttribute("id", "home-div")
 	title.textContent = "JollyBae"
 	title.setAttribute("class", "heading");
+	p.classList.add("text");
 	p.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error aspernatur sed, expedita quae ullam dolorem quas beatae unde iure id alias porro omnis aperiam, molestias recusandae accusantium! Aliquam, quaerat doloremque!"
 
-	div.append(title, p);
+	headingTextContainer.append(title, p);
+	div.append(headingTextContainer);
 	contentDiv.append(div);
-	adjacentHeader.insertAdjacentElement("afterend", contentDiv); // targets the header and inserts #content after it
+	adjacentHeader.insertAdjacentElement("afterend", contentDiv); // inserts the #content div, after the header tag
 }
 
 export { Home };
