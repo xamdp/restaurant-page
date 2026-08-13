ed4c854c167d1929eab13dc6e187cad6971d6f45
xamdp <64017084+xamdp@users.noreply.github.com>
2026-08-13 18:39:57 +0900

    chore: update categories

--- src/category/categories.js
+++ src/category/categories.js
@@ -2,11 +2,15 @@
 export const categories = [
 	{ text: "Breakfast" },
 	{ text: "Lunch" },
-	{ text: "Burger Steak & Chicken Fillet" },
+	{ text: "Burger Steak" },
 	{ text: "Super Meals" },
-	{ text: "Sundaes & Fries" },
-	{ text: "For the Kids Meal" },
+	{ text: "Sundaes" },
+	{ text: "For Kids" },
 	{ text: "Best Sellers" },
 	{ text: "Budget Meal" },
 	{ text: "Dinner" },
+	{ text: "Burgers" },
+	{ text: "Chicken Fillet" },
+	{ text: "Fries" },
+	{ text: "For Teens" },
 ]
