bed2851def86ecda7e13d5e47d94c67a0552df53
xamdp <64017084+xamdp@users.noreply.github.com>
2026-08-18 19:50:03 +0900

    chore: index file smaller than expected

--- src/category/TextLoader.js
+++
@@ -1,8 +0,0 @@
-
-function TextLoader(text) {
-	const p = document.createElement("p");
-	p.text = text;
-	return p;
-}
-
-export { TextLoader };
--- src/container.js
+++
@@ -1,8 +0,0 @@
-
-function Container() {
-	const div = document.createElement("div");
-	div.classList.add("container")
-	document.body.append(div);
-}
-
-export { Container };
