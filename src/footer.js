42912294da51dd783b0b35c9b226e9f4629f3b69
xamdp <64017084+xamdp@users.noreply.github.com>
2026-08-23 09:12:42 +0900

    style: renamed footer content

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,7 @@ function Footer() {
 	const footer = document.createElement("footer");
 	const p = document.createElement("p");
 
-	p.textContent = "Copyright 2026"
+	p.textContent = "Copyright 2026 - JollyBae Restaurant"
 	footer.append(p);
 	document.querySelector(".container").append(footer);
 
